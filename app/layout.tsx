adc779b9778423e03290ec96127c21bcb1a463f9
WestBalance <mr.bluerider3@gmail.com>
2025-10-06 16:03:46 +0300

    Prisma + DB added (ADMIN)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider,} from '@clerk/nextjs'
 import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +26,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ToasterProvider/>
           <ModalProvider/>
         {children}
       </body>
